d399ce916a8f17c533705c7d9273567e31ea5fbb
Antihor <110466159+Antihor@users.noreply.github.com>
2024-02-02 12:30:54 +0000

    Deploying to gh-pages from @ Antihor/GOITHW@735b1c0b60685d07a5e4e0c0ad43dcaa9f24cee3 🚀

---
+++ index.9bcae5a2.js
@@ -0,0 +1,2 @@
+document.querySelector(".button").addEventListener("click",(function(){console.log(iziToast)}));
+//# sourceMappingURL=index.9bcae5a2.js.map
---
+++ index.ad2b9932.js
@@ -0,0 +1,2 @@
+document.querySelector(".button").addEventListener("click",(function(){console.log(iziToast)}));
+//# sourceMappingURL=index.ad2b9932.js.map
